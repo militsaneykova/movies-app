8efed14ef9ea8aa760574fa1fa275d308678ff9f
Asher Shaheen <ashershaheen@gmail.com>
2017-08-18 14:10:33 -0400

    added the movie add form in MovieList Component

--- client/src/components/MoviesList.jsx
+++ client/src/components/MoviesList.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Movie from 'Movie';
+import MovieAddForm from './MovieAddForm';
+import MovieEditForm from './MovieEditForm';
 
 const MoviesList = (props) => {
     return (
         <div className='movies-list'>
+            <MovieAddForm handleMovieSubmit={props.handleMovieSubmit} />
             {props.movieData.map(movie => {
-                return <Movie movie={movie} key={movie.id} />
+                if (props.currentMovieId === movie.id) {
+                    return <Movie movie={movie} key={movie.id} />
+                } else return <Movie movie={Movie} selectEditedMovie={props.selectEditedMovie} key={movie.id} />
             })}
         </div>
     )
