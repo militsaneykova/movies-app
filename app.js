2534f0db19e6592e53861dee167c064e35cf471d
Mitchel Severe <truth531@gmail.com>
2017-08-18 14:34:18 -0400

    uncommented movie routes

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.listen(PORT,()=>{
 // })
 // const authRoutes = require('./routes/auth-routes');
 // app.use('/auth',authRoutes);
-// const movieRoutes = require('./routes/movie-routes');
-// app.use('/movies',movieRoutes);
+const movieRoutes = require('./routes/movie-routes');
+app.use('/movies',movieRoutes);
 
 //Error handler
 app.use('*',(req,res)=>{
