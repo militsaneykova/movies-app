255a3b925f642e3f4d4f4bd02d1d89ef968472e0
Mitchel Severe <truth531@gmail.com>
2017-08-18 10:53:00 -0400

    add state to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      auth: false,
+      user: null,
+      currentPage: none,
+      currentMovieId: null,
+      movieData: null,
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
       </div>
     );
   }
